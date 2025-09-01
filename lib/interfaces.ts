e0ee484be378d73875f7e6c758b35090cbe4b5d8
konstigheter <106750829+ingakonstigheter@users.noreply.github.com>
2025-09-01 15:40:04 +0200

    added more interfaces

--- lib/interfaces.ts
+++ lib/interfaces.ts
@@ -1,16 +1,16 @@
-interface PokemonFull {
+export interface PokemonFull {
   id: number;
   name: string;
   types: PokemonTypes[];
   stats: PokemonStats[];
   sprites: string[];
 }
-interface PokemonShort {
+export interface PokemonShort {
   name: string;
   url: string;
 }
 
-interface PokemonStats {
+export interface PokemonStats {
   base_stat: number;
   effort: number;
   stat: {
@@ -19,8 +19,15 @@ interface PokemonStats {
   };
 }
 
-interface PokemonTypes {
+export interface PokemonTypes {
   type: {
+    name: string;
+    url: string;
+  };
+}
+
+export interface PokemonTypesShort {
+  pokemon: {
     name: string;
     url: string;
   };
