f0ebb325f35c11dcd595bca01fa91c646a81c085
konstigheter <106750829+ingakonstigheter@users.noreply.github.com>
2025-09-01 15:39:32 +0200

    added type fetch

--- lib/data/pokemon.ts
+++ lib/data/pokemon.ts
@@ -1,3 +1,7 @@
+import { PokemonTypesShort } from "@/components/type-option";
+import { MAX_POKEMON } from "../constants";
+import { PokemonFull, PokemonShort } from "../interfaces";
+
 export async function fetchPokemon(id: string): Promise<PokemonFull> {
   try {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
@@ -33,7 +37,9 @@ export async function fetchPokemons(
 
 export async function fetchAllPokemon(): Promise<PokemonShort[]> {
   try {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=25`);
+    const response = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/?limit=${MAX_POKEMON}`
+    );
 
     if (!response.ok) {
       throw new Error("There was a problem, the pokemon's escaped, ");
@@ -41,6 +47,26 @@ export async function fetchAllPokemon(): Promise<PokemonShort[]> {
 
     const data = await response.json();
     return data.results;
+  } catch (e: any) {
+    throw {
+      status: e.status,
+      message: e.statusText || "Network erorr",
+    };
+  }
+}
+
+export async function fetchAllPokemonOfType(
+  type: string
+): Promise<PokemonTypesShort[]> {
+  try {
+    const response = await fetch(`https://pokeapi.co/api/v2/type/${type}`);
+
+    if (!response.ok) {
+      throw new Error("There was a problem, the pokemon's escaped, ");
+    }
+
+    const data = await response.json();
+    return data.pokemon;
   } catch (e: any) {
     throw {
       status: e.status,
