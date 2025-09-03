da445a48cbe9d511221f3ed749340c3782a17c91
konstigheter <106750829+ingakonstigheter@users.noreply.github.com>
2025-09-03 19:15:34 +0200

    post cleanup and commenting

--- components/pokemon-card.tsx
+++ components/pokemon-card.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import Image from "next/image";
-import pokeTypeColors from "@/lib/pokeTypeColors.json";
+import pokeTypeColors from "@/lib/poke-type-colors.json";
 import TypeBadges from "./type-badges";
 import { fetchPokemon } from "@/lib/data/pokemon";
 import { PokemonFull } from "@/lib/interfaces";
 import StatusTable from "./status-table";
-import NameLink from "./name-link";
-import { MAX_POKEMON } from "@/lib/constants";
+import { MAX_POKEMON, SMALLEST_ID } from "@/lib/constants";
 import { notFound } from "next/navigation";
+import PokeLink from "./poke-link";
 
 export default async function PokemonCard({ id }: { id: string | undefined }) {
+  /* Returns not found if id over/under the intervall of pokemons pre fetching */
   if (id) {
-    if (parseInt(id) < 0 || parseInt(id) > MAX_POKEMON) {
+    if (parseInt(id) < SMALLEST_ID || parseInt(id) > MAX_POKEMON) {
       return notFound();
     }
   }
@@ -19,17 +20,21 @@ export default async function PokemonCard({ id }: { id: string | undefined }) {
   const pokemon: PokemonFull | undefined = id
     ? await fetchPokemon(id)
     : undefined;
+
   if (!pokemon) {
     return null;
   }
-  const color =
-    pokeTypeColors[pokemon.types[0].type.name as keyof typeof pokeTypeColors];
+
+  const mainType = pokemon.types[0].type.name;
+  const color = pokeTypeColors[mainType as keyof typeof pokeTypeColors];
 
   return (
     <article className="border-4 rounded w-[264px] h-[396] border-[#637CCE] shadow p-4 bg-[#F0FDFF] grid grid-rows-subgrid row-span-5 gap-1">
+      {/* Name */}
       <h1 className="mx-auto text-3xl row-start-3">
-        <NameLink pokemon={pokemon}></NameLink>
+        <PokeLink id={pokemon.id.toString()} name={pokemon.name}></PokeLink>
       </h1>
+      {/* Image */}
       <figure
         className="w-[100px] border-4 rounded-full flex m-auto p-1 row-start-1"
         style={{ borderColor: color }}>
@@ -40,6 +45,7 @@ export default async function PokemonCard({ id }: { id: string | undefined }) {
           width={96}
           className="object-contain"></Image>
       </figure>
+      {/* number id */}
       <p
         className="rounded-xl w-min px-1 text-white font-bold m-auto row-start-2"
         style={{
@@ -47,8 +53,9 @@ export default async function PokemonCard({ id }: { id: string | undefined }) {
         }}>
         {`#${String(pokemon.id).padStart(4, "0")}`}
       </p>
+      {/* type badges */}
       <TypeBadges types={pokemon.types}></TypeBadges>
-
+      {/* status table */}
       <StatusTable status={pokemon.stats.slice(0, 3)}></StatusTable>
     </article>
   );
