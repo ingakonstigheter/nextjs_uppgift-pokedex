3dc87324bf6fc0578f5d970e410987940a01b7fe
konstigheter <106750829+ingakonstigheter@users.noreply.github.com>
2025-09-03 16:05:21 +0200

    added more dynamic routing

---
+++ components/poke-link.tsx
@@ -0,0 +1,17 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+export default function PokeLink({ id, name }: { name: string; id: string }) {
+  const pathname = usePathname();
+  let path = `pokedex/${id}`;
+  if (pathname === `/pokedex/${id}`) {
+    path = "#";
+  }
+  return (
+    <Link href={path} className="capitalize">
+      {name}
+    </Link>
+  );
+}
