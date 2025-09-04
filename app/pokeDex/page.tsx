e9389e7bd042ae0c9e298b158f5a13e293c6e542
konstigheter <106750829+ingakonstigheter@users.noreply.github.com>
2025-09-04 17:36:42 +0200

    hope this works

--- app/pokeDex/page.tsx
+++ app/pokeDex/page.tsx
@@ -1,17 +1,15 @@
-/* import {
+import {
   fetchAllPokemon,
   fetchAllPokemonOfType,
   fetchPokemons,
-} from "@/lib/data/pokemon"; 
- */
-import React /*, { Suspense }  */ from "react";
-/* import Search from "@/components/search";
+} from "@/lib/data/pokemon";
+import React, { Suspense } from "react";
+import Search from "@/components/search";
 import Image from "next/image";
 import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table";
 import TypeBadges from "@/components/type-badges";
 import TypeOption from "@/components/type-option";
- import PokeLink from "@/components/poke-link";
-*/
+import PokeLink from "@/components/poke-link";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -19,9 +17,9 @@ export const metadata: Metadata = {
   description: "Search and show the original 151 pokemons",
 };
 
-/* async function PokemonList({ query, type }: { query: string; type: string }) {
+async function PokemonList({ query, type }: { query: string; type: string }) {
   let pokemonsData = await fetchAllPokemon();
-  /* if type filter by type 
+  /* if type filter by type */
   if (type !== "") {
     const pokemonsOfType = await fetchAllPokemonOfType(type);
 
@@ -29,7 +27,7 @@ export const metadata: Metadata = {
       pokemonsOfType.some((pokeOfType) => pokeOfType.name === pokemon.name)
     );
   }
-  /* if query filter by query 
+  /* if query filter by query */
   if (query !== "") {
     pokemonsData = pokemonsData.filter((pokemon) =>
       pokemon.name.toLowerCase().includes(query.toLowerCase())
@@ -45,17 +43,17 @@ export const metadata: Metadata = {
           <TableRow
             key={pokemon.id}
             className="grid odd:bg-[#F0FDFF] grid-cols-subgrid col-span-4">
-            {/* id }
+            {/* id */}
             <TableCell className="grid content-center text-2xl">{`#${String(pokemon.id).padStart(4, "0")}`}</TableCell>
 
-            {/* name }
+            {/* name */}
             <TableCell className="grid content-center text-3xl">
               <PokeLink
                 id={pokemon.id.toString()}
                 name={pokemon.name}></PokeLink>
             </TableCell>
 
-            {/* image }
+            {/* image */}
             <TableCell>
               <Image
                 src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${pokemon.id}.png`}
@@ -64,7 +62,7 @@ export const metadata: Metadata = {
                 width={96}></Image>
             </TableCell>
 
-            {/* type/s }
+            {/* type/s */}
             <TableCell className="grid content-center">
               <TypeBadges types={pokemon.types}></TypeBadges>
             </TableCell>
@@ -73,27 +71,27 @@ export const metadata: Metadata = {
       </TableBody>
     </Table>
   );
-} */
-export default async function PokedexPage(/* {
-   searchParams ,
+}
+export default async function PokedexPage({
+  searchParams,
 }: {
   searchParams: Promise<{ [key: string]: string | undefined }>;
-} */) {
-  /*   const { query = "", type = "" } = await searchParams;
-   */
+}) {
+  const params = await searchParams;
+  const { query = "", type = "" } = params;
   return (
     <section className="grid grid-rows-[auto_auto_1fr] min-h-dvh place-items-center bg-gradient-to-r from-blue-100 to-purple-100 p-4">
       <h2 className="text-center text-5xl">Pokedex</h2>
 
-      <div className="p-10 grid "></div>
-      {/*         <Search></Search>
+      <div className="p-10 grid ">
+        <Search></Search>
 
         <TypeOption></TypeOption>
       </div>
 
       <Suspense fallback="Catching all the pokemons...">
         <PokemonList query={query} type={type}></PokemonList>
-      </Suspense> */}
+      </Suspense>
     </section>
   );
 }
