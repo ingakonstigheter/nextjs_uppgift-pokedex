24dc7451127f0e00e1cfd0f25450b7f3eac2f7e4
konstigheter <106750829+ingakonstigheter@users.noreply.github.com>
2025-09-05 13:45:15 +0200

    last change

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,8 @@ import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Pokedex - start",
-  description: "Homepage that displays random Pokemons, Search and show the original 151 pokemons and their stats",
+  description:
+    "Homepage that displays random Pokemons, Search and show the original 151 pokemons and their stats",
 };
 
 export default async function Home({
@@ -24,7 +25,7 @@ export default async function Home({
         <Search></Search>
       </section>
 
-      <section className="bg-gradient-to-r from-blue-100 to-purple-100 grid gap-4 y-center p-8">
+      <section className="bg-gradient-to-r from-blue-100 to-purple-100 grid gap-4 p-8">
         <h2 className="text-5xl text-center">Featured Pokemons</h2>
 
         <Suspense fallback="Loading...">
--- components/feature-list.tsx
+++ components/feature-list.tsx
@@ -7,7 +7,7 @@ export default async function FeatureList() {
   const randomNumbers = uniqueRandomNumbers(4, MAX_POKEMON);
 
   return (
-    <div className="grid md:grid-cols-2 lg:grid-flow-col gap-4 mx-auto [grid-template-rows:repeat(5_1fr)] px-2">
+    <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 mx-auto [grid-template-rows:repeat(5_1fr)] px-2 h-full">
       {randomNumbers.map((id) => (
         <PokemonCard key={id} id={id.toString()}></PokemonCard>
       ))}
